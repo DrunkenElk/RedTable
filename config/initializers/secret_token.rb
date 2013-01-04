f41bdaf5cfe1dba77ff39e800607426e921245b1
DrunkenElk <drunken_elk@2ch.hk>
2013-01-04 22:20:08 +0400

    Add empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RedTable::Application.config.secret_token = 'd934f5a21004ea824cab2824ae7332365b503982030580650637123d76ff9e8001196cd21a600ecb86053200de50ee9172ee34df649c0a485f63b23488ccddb7'
